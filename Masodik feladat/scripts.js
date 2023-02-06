bbd853272ba8142f0a3b89a068ddaf93d7ad1c6e
Marcsi29 <123487871+Marcsi29@users.noreply.github.com>
2023-02-06 18:06:33 +0100

    alap

---
+++ Masodik feladat/scripts.js
@@ -0,0 +1,24 @@
+let color = 'purple';
+let number = 10;
+let world = 'cool';
+
+// $(".eszak").css("background", "purple");
+if(color = 'purple') {
+    $(".eszak").css("background", "purple");
+};
+
+if(number > 100) {
+    $(".kelet").text("wow, de nagy szám");
+}
+else {$(".kelet").text("ez csak egy közönséges szám")};
+
+if(world === 'cool') {
+    $(".del").text("A DOM Ereje");}
+else {$(".nyugat").text("A DOM Ereje")};
+
+
+
+
+
+
+
