5e528297b49fa18f8750e7f329bec2ed48e59487
Marcsi29 <123487871+Marcsi29@users.noreply.github.com>
2023-02-06 19:25:28 +0100

    Update scripts.js

---
+++ Negyedik feladat/scripts.js
@@ -0,0 +1,14 @@
+let peoples = ['Marcsi', 'Bea', 'Kata', 'Pityu', 'Tomi'];
+// $ ("ul").append("li");
+
+// peoples.forEach(myFunction);
+
+$("ul").append('<li> ujabb elem </li>');
+
+
+// peoples.forEach((array) => {
+//   $("ul").append("li");
+// });
+
+
+
